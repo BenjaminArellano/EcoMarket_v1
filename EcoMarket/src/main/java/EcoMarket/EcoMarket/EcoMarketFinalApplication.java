0394adf1f0460a51b6178b3bb910e2fa7d78b099
Bvnjvv <be.arellanor@duocuc.cl>
2025-07-04 13:36:37 -0600

    Primer commit del proyecto

---
+++ EcoMarket/src/main/java/EcoMarket/EcoMarket/EcoMarketFinalApplication.java
@@ -0,0 +1,13 @@
+package EcoMarket.EcoMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoMarketFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoMarketFinalApplication.class, args);
+	}
+
+}
